95d26b838ccd6d039f76490c09a43d3e980f1b12
DianaM411 <dianamunteanu220@gmail.com>
2022-06-06 18:11:37 +0200

    menor mayor

--- src/com/diana/Main.java
+++ src/com/diana/Main.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-
+        //definimos variables
         double totalFuego = 0;
         double totalConstr = 0;
         double totalSalva = 0;
@@ -18,6 +20,8 @@ public class Main {
         double totalDiversos = 0;
         double totalSinInterv = 0;
         double totalServVarios = 0;
+
+        HashMap<Double, String> mayorMenorIncidencias = new HashMap<Double, String>();
 
         try {
             //la carpeta Bomberos
@@ -35,7 +39,7 @@ public class Main {
 
                     String linea;
 //                    Scanner myReader = new Scanner(i);
-                    while ((linea =bf.readLine())!= null) {
+                    while ((linea = bf.readLine()) != null) {
                         String data = linea;
                         String parte[] = data.split(";");//dividimos cada linea en partes separadas por ;
                         totalFuego += Double.parseDouble(parte[3]);
@@ -48,9 +52,25 @@ public class Main {
                     }
                     bf.close();
                 }
+                // Anadir claves y valor al HashMap (tipoIncidencia, numeroTotal)
+                mayorMenorIncidencias.put(totalFuego, "fuego");
+                mayorMenorIncidencias.put(totalConstr, "danoConstr");
+                mayorMenorIncidencias.put(totalSalva, "salvamento");
+                mayorMenorIncidencias.put(totalAgua, "agua");
+                mayorMenorIncidencias.put(totalDiversos, "diversos");
+                mayorMenorIncidencias.put(totalSinInterv, "sinIntervencion");
+                mayorMenorIncidencias.put(totalServVarios, "servVarios");
+
+                ArrayList<Double> claves = new ArrayList<Double>();
+                claves.addAll(mayorMenorIncidencias.keySet());
+                //ordenamos los valores de menor a mayor
+                Collections.sort(claves);
+
+                //sacamos la temparatura minima (la primera posicion)
+                mayorMenorIncidencias.get(claves.get(0));
+                //sacamos la temparatura maxima (ultima posicion)
+                mayorMenorIncidencias.get(claves.get(claves.size() - 1));
             }
-
-
 
         } catch (Exception ex) {//manejamos excepciones
             ex.printStackTrace();
