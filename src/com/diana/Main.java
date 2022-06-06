c9c539f237fa1a10f56e60f8935f831cae7a3905
DianaM411 <dianamunteanu220@gmail.com>
2022-06-06 17:40:27 +0200

    sumar total incidencias

--- src/com/diana/Main.java
+++ src/com/diana/Main.java
@@ -11,6 +11,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
 
+        double totalFuego = 0;
+        double totalConstr = 0;
+        double totalSalva = 0;
+        double totalAgua = 0;
+        double totalDiversos = 0;
+        double totalSinInterv = 0;
+        double totalServVarios = 0;
+
         try {
             //la carpeta Bomberos
             String pwd = System.getProperty("user.dir");//ruta directorio actual de trabajo
@@ -21,7 +29,7 @@ public class Main {
             if (carpetaBomberos.exists()) {
                 File[] contenidoCarpeta = carpetaBomberos.listFiles();
                 for (File i : contenidoCarpeta) {
-                    System.out.println(i); //imprimo las rutas de los archivos de dentro
+
                     FileReader fr = new FileReader(i);
                     BufferedReader bf = new BufferedReader(fr);
 
@@ -29,7 +37,14 @@ public class Main {
 //                    Scanner myReader = new Scanner(i);
                     while ((linea =bf.readLine())!= null) {
                         String data = linea;
-                        System.out.println(data);
+                        String parte[] = data.split(";");//dividimos cada linea en partes separadas por ;
+                        totalFuego += Double.parseDouble(parte[3]);
+                        totalConstr += Double.parseDouble(parte[4]);
+                        totalSalva += Double.parseDouble(parte[5]);
+                        totalAgua += Double.parseDouble(parte[6]);
+                        totalDiversos += Double.parseDouble(parte[7]);
+                        totalSinInterv += Double.parseDouble(parte[8]);
+                        totalServVarios += Double.parseDouble(parte[9]);
                     }
                     bf.close();
                 }
