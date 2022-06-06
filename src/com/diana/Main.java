56653d81a25738c6c467d97e517998b13f9185a8
DianaM411 <dianamunteanu220@gmail.com>
2022-06-06 17:28:15 +0200

    listFiles

--- src/com/diana/Main.java
+++ src/com/diana/Main.java
@@ -1,6 +1,8 @@
 package com.diana;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,17 +12,30 @@ public class Main {
         Scanner lector = new Scanner(System.in);
 
         try {
-            String rutaRelativa = "Bomberos" + File.separator + "2017.csv";
+            //la carpeta Bomberos
             String pwd = System.getProperty("user.dir");//ruta directorio actual de trabajo
-            String rutaAbsoluta = pwd + File.separator + rutaRelativa;// File.separator funciona para Windows y Linux
+            String rutaAbsoluta = pwd + File.separator + "Bomberos";// File.separator funciona para Windows y Linux
+            File carpetaBomberos = new File(rutaAbsoluta);
 
+            //verifico que la carpeta existe y leo los archivos de dentro
+            if (carpetaBomberos.exists()) {
+                File[] contenidoCarpeta = carpetaBomberos.listFiles();
+                for (File i : contenidoCarpeta) {
+                    System.out.println(i); //imprimo las rutas de los archivos de dentro
+                    FileReader fr = new FileReader(i);
+                    BufferedReader bf = new BufferedReader(fr);
 
-            File ficheroALeer = new File(rutaAbsoluta);
-
-            if (ficheroALeer.isFile()) {
-                lector = new Scanner(ficheroALeer);//leemos el fichero
-                System.out.println("Lo leo");
+                    String linea;
+//                    Scanner myReader = new Scanner(i);
+                    while ((linea =bf.readLine())!= null) {
+                        String data = linea;
+                        System.out.println(data);
+                    }
+                    bf.close();
+                }
             }
+
+
 
         } catch (Exception ex) {//manejamos excepciones
             ex.printStackTrace();
